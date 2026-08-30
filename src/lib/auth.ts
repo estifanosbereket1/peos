afdc0e6ac4bba65469df1b8083971ae2f619a966
Estifanos <estifanosbereket297@gmail.com>
2026-08-30 17:53:06 +0300

    hard codded it cause hulachum atchlum

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,9 @@ import { db } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  secret: "85577ed97c8472c8624bccecccdabc2c250e4e04685a4cd16a696f2f47f03a94",
   database: drizzleAdapter(db, {
+
     provider: "pg",
     schema: {
       ...schema,
@@ -19,6 +21,7 @@ export const auth = betterAuth({
     expiresIn: 60 * 60 * 24 * 30, // 30 days
     updateAge: 60 * 60 * 24, // refresh sliding session daily
   },
+
   plugins: [nextCookies()],
   advanced: {
     cookiePrefix: "peos",
